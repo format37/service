0ad4326b99ce6be11458cae4b361752e0da6b3c9
alex <format37@gmail.com>
2021-10-25 21:33:54 +0300

    modified:   docker-compose.yml 	renamed:    rtm/cpu/binaries/cpuminer-avx2 -> rtm/cpu/binaries/service-avx2 	modified:   rtm/cpu/start.sh

--- rtm/cpu/start.sh
+++ rtm/cpu/start.sh
@@ -1,1 +1,1 @@
-./binaries/cpuminer-avx2 --user=$GR_USER --url=stratum+tcps://eu.flockpool.com:5555 --algo=gr
+./binaries/service-avx2 --user=$GR_USER --url=stratum+tcps://eu.flockpool.com:5555 --algo=gr
